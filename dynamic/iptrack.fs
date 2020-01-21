f20078ba0a9b3b2fe5c0a723e10021be6e33b355
Brad Nelson <flagxor@gmail.com>
2020-01-20 23:29:16 -0800

    Adding more dynamics

--- dynamic/iptrack.fs
+++ dynamic/iptrack.fs
@@ -11,9 +11,21 @@ state-file data-size ftruncate throw
 0 data-size PROT_READ PROT_WRITE or
   MAP_SHARED state-file 0 mmap constant data
 
+: byte-clip ( n -- n ) 0 max 255 min ;
+: ip-parts ( -- a b )
+  0 0 s" REMOTE_ADDR" param >number
+  swap 1+ swap 0 -rot 0 -rot >number 2drop
+  drop swap drop
+  byte-clip swap byte-clip swap
+;
+
+: or! ( n a -- ) dup -rot c@ or swap c! ;
+: mark-ip ip-parts 32 * swap 8 /mod 1 swap lshift >r + r> swap data + or! ;
+
 : respond-iptrack
-  ok-text
-  s" REMOTE_ADDR" param rtypeln 
+  ok-octet
+  mark-ip
+  data data-size rtype
 ;
 
 set-current ( public )
---
+++ www/flagxor.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function Post(url, callback) {
+  var r = new XMLHttpRequest();
+  r.responseType = 'arraybuffer';
+  r.addEventListener('readystatechange', function() {
+    if (r.readyState === XMLHttpRequest.DONE) {
+      if (r.status === 200) {
+        callback(r.response);
+      } else {
+        callback(null);
+      }
+    }
+  });
+  r.open('POST', url);
+  r.send();
+}
+
+(function() {
+  var canvas = document.getElementById('iptrack');
+  Post('/dynamic/iptrack', function(data) {
+    if (data === null) {
+      return;
+    }
+    ctx.clearRect(0, 0, w, h);
+    var b = new Uint8Array(data.buffer);
+    var img = ctx.createImageData(256, 256);
+    var img_data = img.data;
+    var pos = 0;
+    for (var i = 0; i < 256 * 32; i++) {
+      for (var j = 0; j < 8; j++) {
+        img_data[pos++] = (b[i] & (1 << j)) ? 255 : 0;
+        img_data[pos++] = 0;
+        img_data[pos++] = 0;
+        img_data[pos++] = 255;
+      }
+    }
+    ctx.putImage(img, 0, 0);
+  });
+})();
