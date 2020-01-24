dc4f27704d44b9850c405080bb8381be39a48215
Brad Nelson <flagxor@gmail.com>
2020-01-24 15:18:59 -0800

    fix

--- www/flagxor.js
+++ www/flagxor.js
@@ -27,37 +27,14 @@ function Post(url, callback) {
     var img = ctx.createImageData(256, 256);
     var img_data = img.data;
     var pos = 0;
-    function sample(x, y) {
-      if (x < 0 || x >= 256 || y < 0 || y >= 256) {
-        return 0;
-      }
-      var i = y * 32 + (x >> 3);
-      var j = x & 7;
-      return (b[i] & (1 << j)) != 0;
-    }
     for (var i = 0; i < 256; i++) {
       for (var j = 0; j < 256; j++) {
-        var a = 0;
-        var b = 0;
-        for (var x = -10; x <= 10; x++) {
-          for (var y = -10; y <= 10; y++) {
-            if (sample(i + x, j + y)) {
-              if (x * x + y * y < 10 * 10) {
-                a++;
-              }
-              if (x * x + y * y < 3 * 3) {
-                b++;
-              }
-            }
-          }
+        if (b[i] & (1 << j)) {
+          trail = 10;
         }
-        if (a) a = 255;
-        if (b) b = 255;
-        a = Math.floor(a * 255 / (21 * 21));
-        b = Math.floor(b * 255 / (7 * 7));
-        img_data[pos++] = Math.min(a, b);
-        img_data[pos++] = Math.min(a / 2, b);
-        img_data[pos++] = b;
+        img_data[pos++] = Math.floor(255 * trail / 10);
+        img_data[pos++] = Math.floor(255 * trail / 20);
+        img_data[pos++] = Math.floor(255 * trail / 30);
         img_data[pos++] = 255;
       }
     }
