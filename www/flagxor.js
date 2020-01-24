955e7f94732efe349ed4be913c88b030ead4c078
Brad Nelson <flagxor@gmail.com>
2020-01-24 15:22:45 -0800

    bright

--- www/flagxor.js
+++ www/flagxor.js
@@ -31,12 +31,12 @@ function Post(url, callback) {
     for (var i = 0; i < 256 * 32; i++) {
       for (var j = 0; j < 8; j++) {
         if (b[i] & (1 << j)) {
-          trail = 10;
+          trail = 20;
         }
         if (trail > 0) {
           --trail;
         }
-        img_data[pos++] = Math.floor(255 * trail / 10);
+        img_data[pos++] = Math.min(255, Math.floor(255 * trail / 10));
         img_data[pos++] = Math.floor(255 * trail / 20);
         img_data[pos++] = Math.floor(255 * trail / 30);
         img_data[pos++] = 255;
