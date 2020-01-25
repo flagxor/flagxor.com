388e53681e7c52b612ca7a0509591c5403b97d2b
Brad Nelson <flagxor@gmail.com>
2020-01-24 19:35:49 -0800

    fix

--- www/disqus.js
+++ www/disqus.js
@@ -1,22 +1,20 @@
 'use strict';
 
-var disqus_shortname = 'flagxor';
-var disqus_config = {};
-
-(function() {
+var disqus_config = function() {
   var title = document.getElementsByTagName('h1')[0];
   var parts = document.location.href.split('/');
   var ident = parts[parts.length - 2];
+  this.shortname = 'flagxor';
+  this.page.identifier = ident;
+  this.page.title = title.innerText;
+  this.page.url = 'https://www.flagxor.com/article/' + ident;
+};
 
-  disqus_config.page = {};
-  disqus_config.page.identifier = ident;
-  disqus_config.page.title = title.innerText;
-  disqus_config.page.url = 'https://www.flagxor.com/article/' + ident;
-
+(function() {
   var s = document.createElement('script');
   s.type = 'text/javascript';
   s.async = true;
-  s.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
+  s.src = 'https://flagxor.disqus.com/embed.js';
   s.setAttribute('data-timestamp', +new Date());
   (document.head || document.body).appendChild(s);
 })();
