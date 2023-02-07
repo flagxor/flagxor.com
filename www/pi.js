aeded4ad8cdb99e26a8ceba5a3cf96e73c8223c7
Brad Nelson <flagxor@gmail.com>
2023-02-06 21:51:00 -0800

    Tweak.

--- www/pi.js
+++ www/pi.js
@@ -2,15 +2,34 @@
 
 (function() {
 
-const SECRET = 'forth';
+const SECRET = 'forth ';
 var secretEntry = '';
 var canvas, ctx;
+var memory = new ArrayBuffer(16 * 1024 * 1024);
+var u8 = new Uint8Array(memory);
+var i32 = new Int32Array(memory);
+var page = 0;
 
 function Draw() {
   ctx.fillStyle = '#000';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = '#ff0';
-  ctx.fillRect(0, 0, canvas.width / 2, canvas.height / 2);
+  ctx.font = '20px consolas, courier';
+  ctx.textBaseline = 'top';
+  ctx.textAlign = 'left';
+  var w = ctx.measureText('W').width;
+  ctx.save();
+  ctx.scale(canvas.width / (w * 64), canvas.height / (20 * 16));
+  for (var j = 0; j < 15; ++j) {
+    ctx.fillStyle = '#f70';
+    ctx.fillText(j + ' : square dup * ;', 0, j * 20);
+    ctx.fillStyle = '#fff';
+    ctx.fillText(j + ' ( testing )', w * 32, j * 20);
+  }
+  ctx.fillStyle = '#07f';
+  for (var j = 0; j < 64; ++j) {
+    ctx.fillText(j % 10, j * w, 15 * 20);
+  }
+  ctx.restore();
 }
 
 function Resize() {
@@ -44,11 +63,11 @@ window.addEventListener('keydown', function(e) {
     }
     return;
   }
-  e.preventDefault();
+//  e.preventDefault();
 });
 
 window.addEventListener('keyup', function(e) {
-  e.preventDefault();
+//  e.preventDefault();
 });
 
 })();
