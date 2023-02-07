644a4999f54852ddbfac57786b03310ef5e66349
Brad Nelson <flagxor@gmail.com>
2023-02-06 21:29:12 -0800

    Tweaks.

--- www/pi.js
+++ www/pi.js
@@ -4,11 +4,32 @@
 
 const SECRET = 'forth';
 var secretEntry = '';
+var canvas, ctx;
+
+function Draw() {
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#ff0';
+  ctx.fillRect(0, 0, canvas.width / 2, canvas.height / 2);
+}
+
+function Resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  Draw();
+}
 
 function Init() {
+  document.title = 'FORTH';
   var wrapper = document.getElementById('wrapper');
   wrapper.parentElement.removeChild(wrapper);
-  document.title = 'FORTH';
+
+  canvas = document.createElement('canvas');
+  document.body.appendChild(canvas);
+  window.addEventListener('resize', Resize);
+  document.body.style.overflow = 'hidden';
+  ctx = canvas.getContext('2d');
+  Resize();
 }
 
 window.addEventListener('keydown', function(e) {
@@ -21,10 +42,13 @@ window.addEventListener('keydown', function(e) {
     if (secretEntry === SECRET) {
       Init();
     }
+    return;
   }
+  e.preventDefault();
 });
 
 window.addEventListener('keyup', function(e) {
+  e.preventDefault();
 });
 
 })();
